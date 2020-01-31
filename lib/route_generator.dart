e0506c0f371fb727570a373b0c704fbdad9dfbb3
luckypal <bigluckypal@gmail.com>
2020-01-30 20:03:19 -0600

    Show onboarding page with 4 images Make router

---
+++ lib/route_generator.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:chatter/src/screens/on_boarding.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    // Getting arguments passed in while calling Navigator.pushNamed
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => OnBoardingWidget());
+      default:
+        // If there is no such named route in the switch statement, e.g. /third
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Error'),
+        ),
+        body: Center(
+          child: Text('ERROR'),
+        ),
+      );
+    });
+  }
+}
---
+++ lib/src/models/on_boarding.dart
@@ -0,0 +1,30 @@
+class OnBoarding {
+  String image;
+  String description;
+
+  OnBoarding({this.image, this.description});
+}
+
+class OnBoardingList {
+  List<OnBoarding> _list;
+
+  List<OnBoarding> get list => _list;
+
+  OnBoardingList() {
+    _list = [
+      new OnBoarding(
+          image: 'assets/images/onboarding0.png',
+          description:
+              'Connect your fan messenger account and use them.'),
+      new OnBoarding(
+          image: 'assets/images/onboarding1.png',
+          description: 'Be yourself, everyone else is already taken.'),
+      new OnBoarding(
+          image: 'assets/images/onboarding2.png',
+          description: 'Businesses and life with one click.'),
+      new OnBoarding(
+          image: 'assets/images/onboarding3.png',
+          description: 'A room with different messenger users.'),
+    ];
+  }
+}
