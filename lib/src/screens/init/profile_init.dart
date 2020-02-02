b20ff89545697465a00fe7ddaaf71ae66cd1eefb
luckypal <bigluckypal@gmail.com>
2020-02-01 23:27:06 -0600

    Make profile init page

--- lib/src/screens/init/profile_init.dart
+++ lib/src/screens/init/profile_init.dart
@@ -1,13 +1,12 @@
-import 'package:chatter/src/screens/init/phone_verify.dart';
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:country_code_picker/country_code_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:flutter/services.dart';
 import 'package:chatter/config/app_config.dart' as config;
 import 'package:chatter/src/utils/ui.dart';
-import 'package:chatter/src/services/phone_verify.dart';
-import 'package:chatter/service_locator.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
 
 class ProfileInitPage extends StatefulWidget {
   @override
@@ -15,9 +14,72 @@ class ProfileInitPage extends StatefulWidget {
 }
 
 class _ProfileInitPageState extends State<ProfileInitPage> {
+  GlobalKey<FormState> _profileSettingsFormKey = new GlobalKey<FormState>();
+  File croppedImgFile;
+  Image croppedImg;
+
+  String userName = "";
+
   @override
   void initState() {
     super.initState();
+  }
+  
+  Future getImage() async {
+    var orgImageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    croppedImgFile = await ImageCropper.cropImage(
+      sourcePath: orgImageFile.path,
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+        // CropAspectRatioPreset.ratio3x2,
+        // CropAspectRatioPreset.original,
+        // CropAspectRatioPreset.ratio4x3,
+        // CropAspectRatioPreset.ratio16x9
+      ],
+      androidUiSettings: AndroidUiSettings(
+        toolbarTitle: 'Cropper',
+        toolbarColor: Theme.of(context).accentColor,
+        toolbarWidgetColor: Colors.white,
+        initAspectRatio: CropAspectRatioPreset.original,
+        lockAspectRatio: true),
+      iosUiSettings: IOSUiSettings(
+        minimumAspectRatio: 1.0,
+      )
+    );
+
+    if (croppedImgFile == null) return;
+
+    setState(() {
+      croppedImg = Image.file(croppedImgFile);
+    });
+  }
+
+  void onDone() {
+    if (_profileSettingsFormKey.currentState.validate()) {
+      //Save croppedImg and userName to server.
+    }
+  }
+
+  // void onLogout() {
+  //   FirebaseAuth.instance.signOut();
+  //   Navigator.pushNamedAndRemoveUntil(context, "/", ModalRoute.withName("/"));
+  // }
+
+  InputDecoration getInputDecoration({String hintText, String labelText}) {
+    return new InputDecoration(
+      hintText: hintText,
+      labelText: labelText,
+      hintStyle: Theme.of(context).textTheme.body1.merge(
+        TextStyle(color: Theme.of(context).focusColor),
+      ),
+      enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).hintColor.withOpacity(0.2))),
+      focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).hintColor)),
+      hasFloatingPlaceholder: true,
+      labelStyle: Theme.of(context).textTheme.body1.merge(
+        TextStyle(color: Theme.of(context).hintColor),
+      ),
+    );
   }
 
   @override
@@ -29,7 +91,7 @@ class _ProfileInitPageState extends State<ProfileInitPage> {
         backgroundColor: Colors.transparent,
         elevation: 0,
         title: Text(
-          'Profile Init',
+          'Init Profile',
           style: Theme.of(context).textTheme.display3,
         ),
       ),
@@ -41,16 +103,59 @@ class _ProfileInitPageState extends State<ProfileInitPage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               SizedBox(height: 15),
-              Text(
-                'Enter your profile data',
-                textAlign: TextAlign.left,
-                style: Theme.of(context).textTheme.body2,
+              
+              Container(
+                alignment: Alignment.center,
+                child: Column(
+                  children: [
+                    croppedImg == null ? FloatingActionButton(
+                      onPressed: getImage,
+                      tooltip: 'Pick Image',
+                      child: Icon(Icons.add_a_photo),
+                    ) :
+                    GestureDetector(
+                      onTap: getImage,
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: croppedImg,
+                        ),
+                      ),
+                    ),
+                    
+                    Text(
+                      'Profile Image',
+                      textAlign: TextAlign.left,
+                      style: Theme.of(context).textTheme.body2,
+                    ),
+                  ],
+                )
+              ),
+
+              SizedBox(height: 15),
+              
+              Form(
+                key: _profileSettingsFormKey,
+                child: Column(
+                  children: <Widget>[
+                    new TextFormField(
+                      style: TextStyle(color: Theme.of(context).hintColor),
+                      keyboardType: TextInputType.text,
+                      decoration: getInputDecoration(hintText: 'ChatterLover', labelText: 'User Name'),
+                      initialValue: "",
+                      validator: (input) => input.trim().length < 3 ? 'Not a valid full name' : null,
+                      onChanged: (input) => setState(() { userName = input; }),
+                    ),
+                  ]
+                ),
               ),
 
               Container(
                 alignment: Alignment.centerRight,
                 child:FlatButton(
-                  onPressed: () {/*FirebaseAuth.instance.signOut();*/},
+                  onPressed: onDone,
                   child: Text(
                     'Done',
                     style: Theme.of(context).textTheme.button,
