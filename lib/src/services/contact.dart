256d4763ae1254bc1d521fcde03b8244b03bda4f
luckypal <bigluckypal@gmail.com>
2020-02-08 20:44:27 -0600

    Find phone numbers from server

--- lib/src/services/contact.dart
+++ lib/src/services/contact.dart
@@ -3,18 +3,21 @@ import 'dart:math';
 import 'package:chatter/service_locator.dart';
 import 'package:chatter/src/models/chatter_contact.dart';
 import 'package:chatter/src/models/user.dart';
+import 'package:chatter/src/services/server.dart';
 import 'package:chatter/src/utils/utilities.dart';
 import 'package:contacts_service/contacts_service.dart';
 import 'package:chatter/src/services/user.dart';
 
 abstract class ContactService {
   UserService userService;
+  ServerService serverService;
   List<ChatterContact> _models;
   List<ChatterContact> get model => _models;
 
   ContactService() {
     // load();
     userService = locator<UserService>();
+    serverService = locator<ServerService>();
   }
 
   Future<List<ChatterContact>> load();
@@ -34,25 +37,26 @@ class ContactServiceImpl extends ContactService {
   @override
   Future<void> filter(Iterable<Contact> contacts) async {
     return new Future<void>(() async {
-      List<String> phoneNumbers = List<String>();
+      // List<String> phoneNumbers = List<String>();
 
       Map<String, Contact> contactTable = Map<String, Contact>();
 
       contacts.forEach((contact) {
         if (contact.phones.length == 0) return;
-
-        // contact.phones.forEach((phoneNumber) => phoneNumbers.add(phoneNumber.value));
+        print('${contact.androidAccountType}  ${contact.displayName}');
         
         contact.phones.forEach((phoneNumber) {
+          print('${phoneNumber.label}  ${phoneNumber.value}');
           String newValue = Utilities.makeStandardPhoneNumber(phoneNumber.value);
-          phoneNumbers.add(newValue);
           contactTable [newValue] = contact;
         });
       });
 
-      _models = new List<ChatterContact>();
-
       int start = 0;
+      _models = new List<ChatterContact>();
+      List<String> phoneNumbers = contactTable.keys.toList(growable: false);
+      // await serverService.findPhones(phoneNumbers);
+
       while(start < phoneNumbers.length) {
         int end = min(start + 10, phoneNumbers.length);
         List<String> subPhoneNumbers = phoneNumbers.sublist(start, end);
--- lib/src/services/server.dart
+++ lib/src/services/server.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 import 'dart:io';
 import 'package:mime/mime.dart';
@@ -7,30 +6,56 @@ import 'package:http_parser/http_parser.dart';
 import 'package:chatter/config/app_config.dart' as config;
 
 abstract class ServerService {
-  Future<Map<String, dynamic>> uploadProfileImage(String phoneNumber, String name, File image);
+  Future<Map<String, dynamic>> uploadProfileImage(
+      String phoneNumber, String name, File image);
+  Future<void> findPhones(List<String> phoneNumbers);
 }
 
 class ServerServiceImpl extends ServerService {
-
   @override
-  Future<Map<String, dynamic>> uploadProfileImage(String phoneNumber, String userName, File image) {
+  Future<Map<String, dynamic>> uploadProfileImage(
+      String phoneNumber, String userName, File image) {
     return new Future(() async {
       try {
-        String url = config.Server.SERVER_URL + "/uploadProfileImage";
+        String url = '${config.Server.SERVER_URL}/uploadProfileImage';
 
         final httpRequest = http.MultipartRequest('POST', Uri.parse(url));
-        httpRequest.fields ["phoneNumber"] = phoneNumber;
-        httpRequest.fields ["userName"] = userName;
+        httpRequest.fields["phoneNumber"] = phoneNumber;
+        httpRequest.fields["userName"] = userName;
 
         if (image != null) {
-          final mimeTypeData = lookupMimeType(image.path, headerBytes: [0xFF, 0xD8]).split('/');
+          final mimeTypeData =
+              lookupMimeType(image.path, headerBytes: [0xFF, 0xD8]).split('/');
 
-          final httpFile = await http.MultipartFile.fromPath('image', image.path,
+          final httpFile = await http.MultipartFile.fromPath(
+              'image', image.path,
               contentType: MediaType(mimeTypeData[0], mimeTypeData[1]));
           httpRequest.fields['ext'] = mimeTypeData[1];
           httpRequest.files.add(httpFile);
         }
-      
+
+        final streamedResponse = await httpRequest.send();
+        final response = await http.Response.fromStream(streamedResponse);
+        if (response.statusCode != 200) {
+          return null;
+        }
+        final Map<String, dynamic> responseData = json.decode(response.body);
+        return responseData;
+      } catch (e) {
+        print(e);
+        return null;
+      }
+    });
+  }
+
+  @override
+  Future<void> findPhones(List<String> phoneNumbers) {
+    return new Future(() async {
+      try {
+        String url = '${config.Server.SERVER_URL}/find/phones';
+        final httpRequest = http.Request('POST', Uri.parse(url));
+        httpRequest.body = json.encode(phoneNumbers);
+
         final streamedResponse = await httpRequest.send();
         final response = await http.Response.fromStream(streamedResponse);
         if (response.statusCode != 200) {
